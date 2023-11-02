ef714255847fd69cd40f23bc3127cd1cdcd1ffc8
Duy Nguyen <duyyb257946@gmail.com>
2023-11-02 11:49:11 +0700

    config .env

--- server.ts
+++ server.ts
@@ -3,14 +3,15 @@ import express from "express";
 import cors from "cors";
 import { routes } from "./src/routes/routes";
 import dataSource from "./src/data-source";
+import dotenv from "dotenv";
 
-const PORT = 4000;
 const app = express();
+dotenv.config();
 dataSource.connect();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(express.json());
 app.use("", routes);
-app.listen(PORT, () => {
-  console.log(`Running http://localhost:${PORT}`);
+app.listen(process.env.PORT, () => {
+  console.log(`Running http://localhost:${process.env.PORT}`);
 });
--- src/data-source.ts
+++ src/data-source.ts
@@ -5,10 +5,8 @@ class DataSource {
   async connect() {
     console.log("start connect");
 
-    const DB_URL =
-      "mongodb+srv://aohota:257946@servertest.gtiolhl.mongodb.net/learn-db";
     try {
-      await mongoose.connect(DB_URL);
+      await mongoose.connect(process.env.DB_URL);
       console.log("connected success");
     } catch (error) {
       console.log("connect false", error);
